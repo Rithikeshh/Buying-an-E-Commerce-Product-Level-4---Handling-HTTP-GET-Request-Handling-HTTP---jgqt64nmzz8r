01ac756e9febac41308df0eac23ce7aaa47b0d56
Rithikeshh <rithikeshh@gmail.com>
2023-12-02 11:23:03 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -14,6 +14,41 @@ app.use(express.json());
 
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
+app.patch('/api/v1/products/:id', (req, res) => {
+    const {id} = req.params;
+    const searchedProduct = products.find((product)=>{
+        return product.id === parseInt(id);
+    })
+    if(searchedProduct){
+        if(searchedProduct.quantity > 0){
+            const index = products.indexOf(searchedProduct);
+            products[index] = {...products[index], quantity: searchedProduct.quantity-1}
+            fs.writeFile(
+                `${__dirname}/data/product.json`,
+                JSON.stringify(products),
+                (err)=>{
+                    res.status(200).send({
+                        "status": "success",
+                        "message": `Thank you for purchasing Product ${id}`,
+                        "product": products[index]
+                    })
+                }
+            )
+        }
+        else{
+            res.status(404).send({
+                "status": "success",
+                "message": `Product ${id}, Out of stock!`
+            })
+        }
+    } 
+    else{
+        res.status(404).send({
+            "status": "failed",
+            "message": "Product not found!"
+        })
+    }
+})
 
 
 
